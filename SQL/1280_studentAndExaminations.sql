a79dcea27ce141ddc680c35f882bb32cbb28f00a
Gajanan Gite <gajanangite30102004@gmail.com>
2026-08-01 14:07:53 +0530

    Create 1280_studentAndExaminations.sql

---
+++ SQL/1280_studentAndExaminations.sql
@@ -0,0 +1,10 @@
+select stu.student_id,stu.student_name,sub.subject_name, count(exam.student_id)as attended_exams 
+from Students  stu
+cross join Subjects  sub
+left join Examinations  exam
+     on stu.student_id=exam.student_id
+     and sub.subject_name=exam.subject_name
+
+group by stu.student_id,stu.student_name,sub.subject_name
+
+order by student_id, subject_name;
