dcc269a50c0eeb2d8583e3898a1689da85bce4da
Gajanan Gite <gajanangite30102004@gmail.com>
2026-08-04 23:32:32 +0530

    Create 3731_missingElement.java

---
+++ 3731_missingElement.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int mn = Integer.MAX_VALUE, mx = Integer.MIN_VALUE;
+        Set<Integer> s = new HashSet<>();
+        for (int x : nums) {
+            mn = Math.min(mn, x);
+            mx = Math.max(mx, x);
+            s.add(x);
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int x = mn + 1; x < mx; ++x) {
+            if (!s.contains(x)) ans.add(x);
+        }
+        return ans;
+    }
+}
